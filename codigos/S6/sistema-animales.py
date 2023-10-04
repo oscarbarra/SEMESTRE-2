7f6535edd10d5875380eef769afcd1e8a04dfc74
oscarbarra <obarra2023@alu.uct.cl>
2023-10-03 22:43:45 -0300

    end the code

--- codigos/S6/sistema-animales.py
+++ codigos/S6/sistema-animales.py
@@ -1,45 +1,37 @@
 class Animal:
-    def __init__(self, nombre, edad, sonido):
+    def __init__(self, nombre, edad):
         self.nombre = nombre
         self.edad = edad
-        self.sonido_animal = sonido
 
-    def sonidoA(self):
-        print(f"soy {self.nombre} y mi sonido es {self.sonido_animal}")
+    def sonido_Animal(self,animal, sonido):
+        print(f"hola soy {self.nombre} el {animal} y mi sonido es {sonido}")
 
     
 class Perro(Animal):
-    def __init__(self, nombre, edad, sonido_animal, raza):
-        super().__init__(nombre, edad, sonido_animal)
-        self.raza = raza 
-
-    def caracteristicas(self):
-        print(f"hola soy {self.nombre} y soy de la raza {self.raza}")
+    def __init__(self, nombre, edad):
+        super().__init__(nombre, edad)
 
 
 class Gato(Animal):
-    def __init__(self, nombre, edad, sonido_animal, color):
-        super().__init__(nombre, edad, sonido_animal)
-        self.color = color
-
-    def caracteristicas(self):
-        print(f"hola soy {self.nombre} y soy de color {self.color}")
+    def __init__(self, nombre, edad):
+        super().__init__(nombre, edad)
 
 
 class Pajaro(Animal):
-    def __init__(self, nombre, edad, sonido_animal, exotico):
-        super().__init__(nombre, edad, sonido_animal)
-        self.exotico = exotico 
-
-    def caracteristicas(self):
-        print(f"hola soy {self.nombre} y vengo {self.exotico}")
+    def __init__(self, nombre, edad):
+        super().__init__(nombre, edad)
 
 
-P1 = Perro("canela", 3, "guau guau", "pastor aleman")
-G1 = Gato("carbon", 5, "miau miau", "negro")
-PJ1 = Pajaro("rio", 6, "pio pio", "directo desde brasil")
+P1 = Perro("Canela", 3)
+G1 = Gato("Copo de nieve", 5)
+A1 = Pajaro("Chimuelo", 6)
 
-for i in (P1, G1, PJ1):
-    i.caracteristicas()
-    i.sonidoA()
-    print("\n")
+
+#todo por herencia
+A1.sonido_Animal("Pajaro", "pio pio")
+input("")
+
+G1.sonido_Animal("Gato", "miau miau")
+input("")
+
+P1.sonido_Animal("Perro", "guau guau")
