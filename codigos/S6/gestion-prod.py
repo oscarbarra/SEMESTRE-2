a325076d3d8d65d7203456750998c171506c7530
oscarbarra <obarra2023@alu.uct.cl>
2023-10-03 22:05:31 -0300

    end the code

--- codigos/S6/gestion-prod.py
+++ codigos/S6/gestion-prod.py
@@ -4,40 +4,43 @@ class Producto:
         self.precio = precio
         self.categoria = categotia
 
+
 class Electronico(Producto):
-    def __init__(self,nombre, precio, categoria, tipo):
+    def __init__(self,nombre, precio, categoria, marca):
         super().__init__(nombre, precio, categoria)
-        self.tipo = tipo
+        self.marca = marca
 
     def mostrar_prod(self):
         print(f"nombre: {self.nombre}\nprecio: {self.precio}")
-        print(f"categoria: {self.categoria}\ntipo: {self.tipo}")
+        print(f"marca: {self.marca}\ncategoria: {self.categoria}")
+
 
 class Alimento(Producto):
-    def __init__(self, nombre, precio, categoria, nacional):
+    def __init__(self, nombre, precio, categoria, procedencia):
         super().__init__(nombre, precio, categoria)
-        self.prod_nacional = nacional
+        self.procedencia = procedencia #atributo unico
 
     def mostrar_prod(self):
         print(f"nombre: {self.nombre}\nprecio: {self.precio}")
-        print(f"categoria: {self.categoria}\nnacional: {self.prod_nacional}")
+        print(f"procedencia: {self.procedencia}\ncategoria: {self.categoria}")
+
 
 class Vestimenta(Producto):
     def __init__(self, nombre, precio, categoria, temporada):
         super().__init__(nombre, precio, categoria)
-        self.temporada = temporada
+        self.temporada = temporada #atributo unico
 
     def mostrar_prod(self):
         print(f"nombre: {self.nombre}\nprecio: {self.precio}")
-        print(f"categoria: {self.categoria}\ntemporada: {self.temporada}")
+        print(f"temporada: {self.temporada}\ncategoria: {self.categoria}")
 
 
-E1 = Electronico("pc", 1000, "Electronico", "gamer")
-A1 = Alimento("arroz", 1000, "alimento", "no")
-V1 = Vestimenta("polera", 5000, "Vestimenta", "verano")
+#inicializacion de los productos
+E1 = Electronico("pc", 1000, "Electronico", "LG")
+A1 = Alimento("arroz", 1000, "Alimento", "Alemania")
+V1 = Vestimenta("polera", 5000, "Vestimenta", "Verano")
 
-
-for i in (E1, A1, V1):
-    i.mostrar_prod()
-    print("\n")
-    
+#salida de datos
+for producto in (E1, A1, V1):
+    producto.mostrar_prod()
+    input("")
