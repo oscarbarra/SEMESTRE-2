6cd44e96531cde4949a1769d6727489c0ffc84bb
oscarbarra <obarra2023@alu.uct.cl>
2023-11-08 13:42:27 -0300

    PAULO

---
+++ PROGRAMACION-2/S12/actividad-c++/hola.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int num1, num2;
+
+    std::cout << "ingresar un numero: "; std::cin >> num1;
+    std::cout << "ingresar un numero: "; std::cin >> num2;
+
+    std::cout << "la suma de los numeros " << num1 << " " << num2 << " es: hola mundo" << std::endl;
+
+    return 0;
+}
